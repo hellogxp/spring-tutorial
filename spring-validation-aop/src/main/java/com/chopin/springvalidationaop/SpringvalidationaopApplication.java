043b602a373c238127e2a440bd001fab29aef66b
燕衡 <xueping.gxp@alibaba-inc.com>
2021-09-08 14:13:03 +0800

    spring validation aop

---
+++ spring-validation-aop/src/main/java/com/chopin/springvalidationaop/SpringvalidationaopApplication.java
@@ -0,0 +1,13 @@
+package com.chopin.springvalidationaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvalidationaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringvalidationaopApplication.class, args);
+    }
+
+}
---
+++ spring-validation-aop/src/test/java/com/chopin/springvalidationaop/SpringvalidationaopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chopin.springvalidationaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvalidationaopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
